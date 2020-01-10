ad7066fba3ec4026e3ec6190d67edd4ea64179c4
Jonathan Meraz <JMERAZ@compsourcemutual.com>
2020-01-10 15:18:15 -0600

    fixes to di-assignment

--- src/main/java/guru/springframework/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGreetingService.java
@@ -1,5 +1,6 @@
 package guru.springframework.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,13 @@ public class PrimaryGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
 
+    public PrimaryGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+    
     @Override
     public String sayGreeting() {
         return greetingRepository.getEnglishGreeting();
     }
+
 }
